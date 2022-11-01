f6558dcb740ce6db6239bc931a6707651de30526
Omar <oadrian38@gmail.com>
2022-10-31 23:00:51 -0700

    Se ha creado el filtro de busqueda por texto y agregago la fase beta de un Modal para mostrar detalles de pkmn

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,27 +1,38 @@
 // Contenedor principal de tarjetas/cards
-
 let cardContainer = document.getElementById('pkmnContainer')
 
 async function returnPkmn(num) {
-    let pkmnData = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)).json()
-    return pkmnData
+    try {
+        let pkmnData = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)).json()
+        return pkmnData
+    }catch(err){
+        console.log('Se ha generado un error ', err)
+    }
 }
-async function returnPkmnv2() {
-    let pkmnData = await (await fetch('https://pokeapi.co/api/v2/pokemon/2')).json()
-    console.log(pkmnData)
+async function returnPkmnForData() {
+    let pkmnData = await (await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=900')).json()
+    // console.log(pkmnData)
+    let pkmnAll = []
+    pkmnData.results.forEach(elem => {
+        pkmnAll.push(elem.name)
+    })
+    return pkmnAll
 }
-async function renderPkmns(num, cb) {
-    let pkmnData = await cb(num)
+async function renderPkmns(num) {
+    let pkmnData = await returnPkmn(num)
     let pkmnCard = document.createElement('div');
-    pkmnCard.setAttribute('name', pkmnData.order)
-    pkmnCard.setAttribute('id', pkmnData.order)
+    pkmnCard.setAttribute('name', pkmnData.id)
+    pkmnCard.setAttribute('id', pkmnData.id)
+    // pkmnCard.setAttribute('onclick', renderOnModal)
+    pkmnCard.setAttribute('data-bs-toggle', 'modal')
+    pkmnCard.setAttribute('data-bs-target', '#renderModalView')
     pkmnCard.className = 'card d-flex flex-column align-items-center p-2'
     pkmnCard.innerHTML = `
                         <div class="card__mainInfo d-flex flex-row align-items-center justify-content-between">
                             <p class="card__name">${pkmnData.name}</p>
                             <p class="card__id">#000</p>
                         </div>
-                        <img src="${pkmnData.sprites.front_default}" alt="Imagen de Pkmn" class="card__img">
+                        <img src="${pkmnData.sprites.front_default}" alt="Imagen de Pkmn" class="card__img" onclick='renderOnModal(event)'>
                         `
     let pkmnTypes = document.createElement('div');
     pkmnTypes.className = 'card__types d-flex flex-row align-items-center justify-content-evenly'
@@ -37,6 +48,35 @@ async function renderPkmns(num, cb) {
     cardContainer.append(pkmnCard)
 }
 
-renderPkmns(1, returnPkmn)
+renderPkmns(25)
 
-returnPkmnv2()
+// onkeypressSearch
+// Filtrado y renderizado de tarjetas pkmn
+async function onkeypressSearch(e) {
+    cardContainer.innerHTML = ''
+    let pkmnNamesAll = await returnPkmnForData()
+    let filterNames = pkmnNamesAll.filter((elem, index) => {
+        return elem.startsWith(e.target.value)
+    })
+    if(e.target.value !== ''){
+        pkmnNamesAll.forEach((elem, index) => {
+            if(elem.startsWith(e.target.value)){
+                renderPkmns(index + 1)
+            }
+        })
+    }else{
+        cardContainer.innerHTML = ''
+    }
+    // console.log(filterNames)
+}
+
+
+// Render y configuracion sobre el Modal
+let modal__pkmnName = document.getElementById('modal__pkmnName')
+let modal__pkmnGeneralInfo = document.getElementById('modal__pkmnGeneralInfo')
+let renderOnModal = async (e) => {
+    let pkmnData = await returnPkmn(e.target.parentElement.id)
+    modal__pkmnName.innerHTML = `${pkmnData.name}`
+    modal__pkmnGeneralInfo.innerHTML = `<img src='${pkmnData.sprites.front_default}'/>`
+}
+
